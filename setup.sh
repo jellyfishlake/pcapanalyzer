ef146242ee469736a7b07a2e1479e2dcb53bc09d
basti <ch1hocklo@tuta.io>
2021-08-29 19:12:35 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ pip install pandas jinja2 pycaret xhtml2pdf argparse datetime
 wget https://tranalyzer.com/download/tranalyzer/tranalyzer2-0.8.11lmw2.tar.gz
 tar xzf tranalyzer2-0.8.11lmw2.tar.gz
 ./$(ls -d */ | grep tranalyzer)setup.sh -T -e
-t2build -b plugins.text
-source ~/.bashrc
+
+
