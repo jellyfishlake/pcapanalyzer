0db7924650eef60d4bb8cf7fe370d120a8bd7599
basti <ch1hocklo@tuta.io>
2021-08-29 20:01:15 +0200

    readf2

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@
 pip install pandas jinja2 pycaret xhtml2pdf argparse datetime
 wget https://tranalyzer.com/download/tranalyzer/tranalyzer2-0.8.11lmw2.tar.gz
 tar xzf tranalyzer2-0.8.11lmw2.tar.gz
-./$(ls -d */ | grep tranalyzer)setup.sh -T -e
+./$(ls -d */ | grep tranalyzer)setup.sh -T
 
-cat <<EOT >> ~/.bashrc
-if [ -f "$PWD/$(ls -d */ | grep tranalyzer)scripts/t2_aliases" ]; then
-    . "$PWD$(ls -d */ | grep tranalyzer)scripts/t2_aliases"
-fi
-EOT
+
